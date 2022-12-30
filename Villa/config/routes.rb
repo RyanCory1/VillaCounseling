4ecfbbf180dba207bc39cb51c6dac78935d53e8d
Ryan Cory <rynocory@comcast.net>
2022-12-29 19:52:52 -0600

    navbar and bootstrap added

--- Villa/config/routes.rb
+++ Villa/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "about", to: "about#index"
   root to: "main#index"
   get "meet", to: "meet#index"
   get "pricing", to: "pricing#index"
